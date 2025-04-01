ea19a6eab7d106800497fada55e64169ee78e2b4
Jesus Sanchez de la Riva <sanchezdelariva.jesus@jgvaldemora.org>
2025-04-01 11:49:06 +0200

    Trabajo endes, clase modificada

--- Endes-Git/src/A04_Endes/JS.java
+++ Endes-Git/src/A04_Endes/JS.java
@@ -3,5 +3,6 @@ package A04_Endes;
 public class JS {
     public void main(String[] args){
         System.out.println("Hola Git");
+        System.out.println("Estoy modificando el odigo desde netbeans");
     }
 }
